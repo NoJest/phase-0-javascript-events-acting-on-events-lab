2333cce1fbdff638e18937e932c85e65f4f6326a
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-09-23 09:57:00 -0400

    this was weird

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+// Grab the dodger
+const dodger = document.getElementById("dodger");
+// Building our ModeDodgerLeft() function (+check on current position)
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  // Wire it to even listener 
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  // building our movedodget right function
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  // Event listener right
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
